c26b8362c2f9559d7e4cb12ce95a0616f0f164c2
LinneaMay <Linneamay@gilbertson.net>
2024-02-13 14:01:57 -0600

    #5 added todo.ts and todo.service.ts

---
+++ client/src/app/todos/todo.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Todo } from './todo';
+import { map } from 'rxjs/operators';
+import { Company } from '../company-list/company';
+
+@Injectable({
+  providedIn: `root`
+})
+export class TodoService {
+//the URL for the todos part of the server API.
+readonly todoUrl: string = `${environment.apiUrl}todos`;
+private readonly categoryKey = 'category'
+private readonly ownerKey = 'owner'
+private readonly bodyKey = 'body'
+
+constructor(private httpClient: HttpClient) {
+}
+
+getTodos() : Observable<todo[]> {
+  let httpParams: HttpParams = new HttpParams();
+
+
+  return this.httpClient.get<Todo[]>(this.todoUrl, {
+    params: httpParams,
+  });
+}
+
+/**
+   * Get the `Todo` with the specified ID.
+   *
+   * @param id the ID of the desired todo
+   * @returns an `Observable` containing the resulting todo.
+   */
+getTodoById(id: string): Observable<Todo> {
+  // The input to get could also be written as (this.todoUrl + '/' + id)
+  return this.httpClient.get<Todo>(`${this.todoUrl}/${id}`);
+}
+
+
+
+
+
+
+}
